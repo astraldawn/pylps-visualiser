b01720cfb316b01f9aaa7479a9b93759e1010a61
Mark <mark.lee.chu.yong@gmail.com>
2018-05-22 17:02:47 +0100

    initial commit for vis code

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pylps_vis',
+    version='0.0.1',
+    description='LPS in python',
+)
+
+# setup(
+#     name='pylps_helper',
+#     version='0.0.1',
+#     description='pylps helper',
+#     entry_points={
+#         'console_scripts': [
+#             'pylps=pylps_helper.command_line:main'
+#         ]
+#     },
+# )
